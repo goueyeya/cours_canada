29245172508ae3081a3ef558d6869891959ab2dd
Gaëtan Oueyeya <gtoueyeya@gmail.com>
2023-02-19 16:44:19 -0500

    rest

--- inf5190/TP/Ateliers/06-REST/database.py
+++ inf5190/TP/Ateliers/06-REST/database.py
@@ -21,7 +21,17 @@ class Database:
         livres = cursor.fetchall()
         return [(livre[0], livre[1]) for livre in livres]
 
-    def get_livre_by_id(self, id):
+    def get_livre_by_id(self, id_book):
         cursor = self.get_connection().cursor()
-        cursor.execute("select * from livre where id = ?", id)
-        return [i for i in cursor]
+        cursor.execute("select * from livre where id = ?", (id_book,))
+        return [(i[0], i[1], i[2], i[3], i[4], i[5]) for i in cursor]
+
+    def nb_livres(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select count(id) from livre")
+        return cursor.fetchone()[0]
+
+    def add_book(self, titre, auteur, annee, nb_pages, nb_chap):
+        cursor = self.get_connection().cursor()
+        cursor.execute("insert into livre (titre, auteur, annee_publi, nb_pages, nb_chap) VALUES (?, ?, ?, ?, ?)",
+                       (titre, auteur, annee, nb_pages, nb_chap))
--- inf5190/TP/Ateliers/06-REST/index.py
+++ inf5190/TP/Ateliers/06-REST/index.py
@@ -30,12 +30,31 @@ def display_all_books():
         return jsonify(data)
 
 
-@app.route("/api/livre/<id>", methods=["GET"])
-def display_book(id):
+@app.route("/api/livre/<id_book>", methods=["GET"])
+def display_book(id_book):
     if request.method == "GET":
-        livre = get_db().get_livre_by_id(id)
-        data = [{"id": livre[0], "titre": livre[1], "auteur": livre[2], "annee_publi": livre[3], "nb_pages": livre[4], "nb_chap": livre[5]}]
-        return jsonify(data)
+        livre = get_db().get_livre_by_id(id_book)
+        nb_book = get_db().nb_livres()
+        if id_book > nb_book:
+            return 404
+        else:
+            data = [{"id": livre[0][0], "titre": livre[0][1], "auteur": livre[0][2], "annee_publi": livre[0][3],
+                     "nb_pages": livre[0][4], "nb_chap": livre[0][5]}]
+            return jsonify(data)
+
+@app.route("/api/livre", methods=["POST"])
+def add_book():
+    if request.method == "POST":
+        data = request.get_json()
+        if ("titre" and "auteur" and "annee" and "nb_pages" and "nb_chap") in data:
+            if type(data["annee"]) == int and type(data["nb_pages"]) == int and type(data["nb_chap"]) == int:
+                get_db().add_book(data["titre"], data["auteur"], data["annee"], data["nb_pages"], data["nb_chap"])
+                return 201
+            else:
+                return 400
+        else:
+            return 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
