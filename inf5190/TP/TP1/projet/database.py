949f0cd962cf8fb320b73101d34a05b85924a3a3
Oueyeya Gaëtan <107945219+goueyeya@users.noreply.github.com>
2023-03-03 01:50:47 -0500

    TP1 FIN

--- inf5190/TP/TP1/projet/database.py
+++ inf5190/TP/TP1/projet/database.py
@@ -17,23 +17,44 @@ class Database:
 
     def nb_articles(self):
         cursor = self.get_connection().cursor()
-        nb = cursor.execute("select count(id) from article").fetchone()[0]
+        nb = cursor.execute("select count(id_art) from article").fetchone()[0]
         return nb
 
-    def get_article_by_id(self, identifiant):
+    def get_article_by_identifiant(self, identifiant):
         cursor = self.get_connection().cursor()
-        article = cursor.execute("select titre, identifiant, auteur, date_publication, paragraphe from article "
+        article = cursor.execute("select titre, auteur, date_publication, paragraphe from article "
                                  "where identifiant = ?", [identifiant]).fetchone()
         return article
 
     def get_article_by_search(self, search):
         cursor = self.get_connection().cursor()
-        articles = cursor.execute("select * from article like %?%", [search]).fetchall()
-        return[(i[0], i[1], i[2], i[3], i[4], i[5]) for i in articles]
+        articles = cursor.execute("select titre, identifiant, date_publication from article where date_publication "
+                                  "<= date() and titre in (select titre from article where titre like '%'||?||'%' or "
+                                  "paragraphe like '%'||?||'%')",
+                                  (search, search)).fetchall()
+        return [(i[0], i[1], i[2]) for i in articles]
 
     def get_last_insert(self):
         cursor = self.get_connection().cursor()
-        articles = cursor.execute("select * from article where date_publication <= date('now','localtime')"
-                                  "order by date_publication desc limit 5 ").fetchall()
-        return[(i[0], i[1], i[2], i[3], i[4], i[5]) for i in articles]
+        articles = cursor.execute("select titre, identifiant, auteur, date_publication, paragraphe from article "
+                                  "where date_publication <= date() order by date_publication "
+                                  "desc limit 5 ").fetchall()
+        return [(i[0], i[1], i[2], i[3], i[4]) for i in articles]
 
+    def get_all_articles(self):
+        cursor = self.get_connection().cursor()
+        articles = cursor.execute("select titre, identifiant, date_publication from article").fetchall()
+        return [(i[0], i[1], i[2]) for i in articles]
+
+    def update_article(self, id_art, new_titre, new_paragraphe):
+        cursor = self.get_connection().cursor()
+        cursor.execute("update article set titre = ?, paragraphe = ? where identifiant = ?", (new_titre, new_paragraphe,
+                                                                                              id_art))
+        self.get_connection().commit()
+
+    def create_article(self, titre, identifiant, auteur, date_publication, paragraphe):
+        cursor = self.get_connection().cursor()
+        article = (titre, identifiant, auteur, date_publication, paragraphe)
+        cursor.execute("INSERT INTO article (titre, identifiant, auteur, date_publication, paragraphe) "
+                       "VALUES (?, ?, ?, ?, ?)", article)
+        self.get_connection().commit()
--- inf5190/TP/TP1/projet/index.py
+++ inf5190/TP/TP1/projet/index.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, abort, url_for
 from flask import render_template
 from flask import request
 from flask import redirect
 from flask import g
 from database import Database
+from datetime import date
 
 app = Flask(__name__)
 
+
 def get_db():
-    db = getattr(g,"_database",None)
+    db = getattr(g, "_database", None)
     if db is None:
         g._database = Database()
     return g._database
@@ -16,49 +18,87 @@ def get_db():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"),404
+    return render_template("404.html", error=e), 404
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
-    nb_articles = get_db().nb_articles()
-    if nb_articles < 1:
-        return render_template("index.html",msg="Aucun article pour le moment...")
-    else:
-        articles= get_db().get_last_insert()
-        results=({"titre": i[0], "identifiant": i[1], "auteur": i[2],"date_publi": i[3],"paragraphe": i[4]} for i in articles)
-        return render_template("index.html", args=results)
-        
+    articles = get_db().get_last_insert()
+    results = ({"titre": i[0], "identifiant": i[1], "auteur": i[2], "date_publi": i[3],
+                "paragraphe": i[4]} for i in articles)
+    return render_template("index.html", articles=results)
 
-@app.route("/recherche/<search>", methods=["GET"])
-def search_article(search):
-    search_db = get_db().get_article_by_search(search)
-    if search_db is None:
-        return render_template("recherche.html", no_args="")
-    else:
-        results=({"titre": i[0], "identifiant": i[1], "auteur": i[2],"date_publi": i[3],"paragraphe": i[4]} for i in search_db)
-        return render_template("recherche.html",args=results)
+
+@app.route("/recherche", methods=["GET"])
+def search_article():
+    search_db = get_db().get_article_by_search(request.args['search'])
+    results = ({"titre": i[0], "identifiant": i[1], "date_publi": i[2]} for i in search_db)
+    return render_template("recherche.html", articles=results, search=request.args['search'])
 
 
 @app.route("/article/<identifiant>", methods=["GET"])
 def display_article(identifiant):
-    article_db = get_db().get_article_by_id(identifiant)
-    article = {"titre": article_db[0], "identifiant":article_db[1], "auteur": article_db[2],"date_publi": article_db[3],
-              "paragraphe": article_db[4]}
-    return render_template("un_article.html", livre=article)
+    article_db = get_db().get_article_by_identifiant(identifiant)
+    if article_db is None:
+        abort(404)
+    article = {"titre": article_db[0], "auteur": article_db[1], "date_publi": article_db[2],
+               "paragraphe": article_db[3]}
+    return render_template("un_article.html", article=article)
 
 
-"""
-@app.route("/admin",methods=["GET"])
+@app.route("/admin", methods=["GET"])
 def admin():
-    if request.method == "GET":
+    article_db = get_db().get_all_articles()
+    results = ({"titre": i[0], "identifiant": i[1], "date_publi": i[2]} for i in article_db)
+    return render_template("page_admin.html", articles=results)
 
 
-@app.route("/admin-nouveau",methods=["GET"])
+@app.route("/admin-modification/<identifiant>", methods=["GET", "POST"])
+def admin_modif(identifiant):
+    article_db = get_db().get_article_by_identifiant(identifiant)
+    results = {"titre": article_db[0], "auteur": article_db[1], "date_publi": article_db[2],
+               "paragraphe": article_db[3], "identifiant": identifiant}
+    if request.method == 'POST':
+        titre = request.form['titre']
+        paragraphe = request.form['paragraphe']
+        if not titre or not paragraphe:
+            return render_template("modif_article.html", article=results, error='Tous les champs sont requis !')
+        if len(paragraphe) > 500:
+            return render_template("modif_article.html", article=results, errorlen='Limite de 500 caractères !')
+        else:
+            get_db().update_article(identifiant, titre, paragraphe)
+            return redirect(url_for('admin'))
+    return render_template("modif_article.html", article=results)
+
+
+@app.route("/admin-nouveau", methods=["GET", "POST"])
 def admin_nouveau():
-    if request.method == "GET":
-"""
+    if request.method == 'POST':
+        titre = request.form['titre']
+        identifiant = request.form['identifiant']
+        auteur = request.form['auteur']
+        date_publi = request.form['date_publi']
+        paragraphe = request.form['paragraphe']
+        article_bak = {"titre": request.form['titre'], "identifiant": request.form['identifiant'],
+                       "auteur": request.form['auteur'], "date_publi": request.form["date_publi"],
+                       "paragraphe": request.form['paragraphe']}
+        if not titre or not identifiant or not auteur or not date_publi or not paragraphe:
+            return render_template("ajout_article.html", article=article_bak, error='Tous les champs sont requis !')
+        elif len(titre) > 100:
+            return render_template("ajout_article.html", article=article_bak, errorti='Limite de 100 caractères !')
+        elif len(identifiant) > 500:
+            return render_template("ajout_article.html", article=article_bak, errorpid='Limite de 50 caractères !')
+        elif len(auteur) > 100:
+            return render_template("ajout_article.html", article=article_bak, erroraut='Limite de 100 caractères !')
+        elif len(paragraphe) > 500:
+            return render_template("ajout_article.html", article=article_bak, errorpar='Limite de 500 caractères !')
+        elif not date.fromisoformat(date_publi):
+            return render_template("ajout_article.html", article=article_bak, errordat='Format de date incorrect !')
+        else:
+            get_db().create_article(titre, identifiant, auteur, date_publi, paragraphe)
+            return redirect(url_for('admin'))
+    return render_template("ajout_article.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
